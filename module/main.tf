c78a19125cdcc071d080c940bcc5320f4c4b232b
rberlind <roger@hashicorp.com>
2019-05-30 14:42:02 -0500

    added module

---
+++ module/main.tf
@@ -0,0 +1,35 @@
+variable "cidr_1" {}
+
+variable "cidr_2" {}
+
+variable "cidr_3" {}
+
+variable "cidr_4" {}
+
+resource "aws_security_group" "test_nested" {
+  name        = "test_nested"
+  description = "test"
+  vpc_id      = "vpc-0ef62214b4149796f"
+
+  tags {
+    name = "webapp00"
+  }
+
+  egress {
+    description = "dns servers"
+    from_port   = 53
+    to_port     = 53
+    protocol    = "udp"
+    cidr_blocks = ["${var.cidr_3}"]
+    self        = false
+  }
+
+
+  ingress {
+    description = "internet facing lbs"
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks     = ["${var.cidr_4}"]
+  }
+}
